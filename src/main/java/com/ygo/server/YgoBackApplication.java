91d3347acef0b07f8b5c0a5d0295a95fa57076d3
DUJEONG\prixi <dujeong@gaeasoft.co.kr>
2024-01-23 10:37:17 +0900

    init

---
+++ src/main/java/com/ygo/server/YgoBackApplication.java
@@ -0,0 +1,13 @@
+package com.ygo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YgoBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ygo/server/YgoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygo.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YgoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
