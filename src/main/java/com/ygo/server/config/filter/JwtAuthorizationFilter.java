cb7ca1d552f47d46664241abf0ebbef45ac9b2e9
Daun Jeong <prixia@naver.com>
2024-05-15 21:15:56 +0900

    통계 화면 로그인 체크 제외

--- src/main/java/com/ygo/server/config/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ygo/server/config/filter/JwtAuthorizationFilter.java
@@ -32,7 +32,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 "/user/find/",
                 "/user/login",
                 "/user/refresh",
-                "/user/signup"
+                "/user/signup",
+                "/statistics/"
         );
 
         // 2. 토큰이 필요하지 않은 API URL의 경우 => 로직 처리 없이 다음 필터로 이동
