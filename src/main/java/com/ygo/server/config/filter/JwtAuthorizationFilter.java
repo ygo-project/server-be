161c9c48d91964002be4bf33a392014b7f1d7db6
Daun Jeong <prixia@naver.com>
2024-06-11 16:43:33 +0900

    덱 목록 조회 auth 필터 제외

--- src/main/java/com/ygo/server/config/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ygo/server/config/filter/JwtAuthorizationFilter.java
@@ -33,7 +33,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 "/user/login",
                 "/user/refresh",
                 "/user/signup",
-                "/statistics/"
+                "/statistics/",
+                "/deck/list"
         );
 
         // 2. 토큰이 필요하지 않은 API URL의 경우 => 로직 처리 없이 다음 필터로 이동
